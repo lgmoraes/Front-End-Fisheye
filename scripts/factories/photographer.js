2f52e452ee1a65f2943369c6a7925d1f8534483b
Louis Moraes <lmoraes@hotmail.fr>
2022-04-20 15:05:22 +0200

    Feat : Add photographer informations

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+	const { id, name, portrait, city, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement("article");
+		article.className = "photographer";
+
+		const img = document.createElement("img");
+		img.setAttribute("src", picture);
+
+		const h2 = document.createElement("h2");
+		h2.textContent = name;
+
+		const p_city = document.createElement("p");
+		p_city.textContent = city;
+		p_city.className = "photographer__city";
+
+		const p_tagline = document.createElement("p");
+		p_tagline.textContent = tagline;
+		p_tagline.className = "photographer__tagline";
+
+		const p_price = document.createElement("p");
+		p_price.textContent = `${price}€/jour`;
+		p_price.className = "photographer__price";
+
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(p_city);
+		article.appendChild(p_tagline);
+		article.appendChild(p_price);
+		return article;
+	}
+	return { id, name, portrait, city, tagline, price, getUserCardDOM };
 }
