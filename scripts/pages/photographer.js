474c3153213bd0c34d4c553c0cf732d3e13edeb2
Louis Moraes <lmoraes@hotmail.fr>
2022-05-04 01:23:20 +0200

    Feat photographer : Add insert

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -11,11 +11,11 @@ async function displayData() {
 	const photographerModel = photographerFactory(data.photographer);
 	const photographerBannerDOM = photographerModel.getUserBannerDOM();
 
-	/* DISPLAY BANNER */
+	/* BANNER */
 
 	$photographBanner.parentElement.replaceChild(photographerBannerDOM, $photographBanner);
 
-	/* DISPLAY THUMBNAILS */
+	/* THUMBNAILS */
 
 	data.medias.forEach((media) => {
 		const mediaModel = mediaFactory(media, data.photographer);
@@ -32,6 +32,17 @@ async function displayData() {
 			video.pause();
 		});
 	});
+
+	/* INSERT */
+
+	const likes = data.medias.reduce((cumul, media) => cumul + media.likes, 0);
+
+	const insertDOM = document.querySelector(".insert");
+	insertDOM.className = "insert";
+	insertDOM.innerHTML = `
+		<p>${likes} ❤</p>
+		<p>${data.photographer.price}€ / jour</p>
+	`;
 }
 
 async function init() {
