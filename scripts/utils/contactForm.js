58ebd332145c449f9fccf9abd64509aceb8e1434
Louis Moraes <lmoraes@hotmail.fr>
2022-05-11 06:21:51 +0200

    Feat : form is now accessible

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,25 +1,53 @@
 export default { init, open, close, send };
 
 function init(title) {
+	const $modal = document.querySelector(".modal");
+	const $modal__button = document.querySelector(".modal__button");
+	const $modal__close = document.querySelector(".modal__close");
+
 	document.querySelector("#modal__header-name").innerHTML = title;
-	document.querySelector(".modal__close").addEventListener("click", close);
-	document.querySelector(".modal__button").addEventListener("click", (e) => send(e));
+	document.querySelector("#modal__title").setAttribute("aria-label", `Contact me ${title}`);
+
+	$modal__button.addEventListener("click", (e) => send(e));
+	$modal__close.addEventListener("click", close);
+	$modal__close.addEventListener("keyup", (e) => {
+		if (e.key !== " ") return false;
+
+		close();
+	});
+	$modal.addEventListener("keydown", (e) => {
+		if (e.key !== "Tab") return false;
+
+		if (e.target === $modal && e.shiftKey) {
+			$modal__close.focus();
+			e.preventDefault();
+		} else if (e.target === $modal__close && !e.shiftKey) {
+			$modal.focus();
+			e.preventDefault();
+		}
+	});
 }
 
 function open() {
-	const modal = document.getElementById("modal");
-	const overlay = document.querySelector(".overlay");
+	const $body = document.querySelector("body");
+	const $modal = document.querySelector(".modal");
+	const $overlay = document.querySelector(".overlay");
 
-	modal.setAttribute("aria-hidden", "false");
-	overlay.setAttribute("aria-hidden", "false");
+	$modal.setAttribute("aria-hidden", "false");
+	$overlay.setAttribute("aria-hidden", "false");
+	$body.classList.add("no-scroll");
+
+	$modal.focus();
 }
 
 function close() {
-	const modal = document.getElementById("modal");
-	const overlay = document.querySelector(".overlay");
+	const $body = document.querySelector("body");
+	const $modal = document.querySelector(".modal");
+	const $overlay = document.querySelector(".overlay");
 
-	modal.setAttribute("aria-hidden", "true");
-	overlay.setAttribute("aria-hidden", "true");
+	$modal.setAttribute("aria-hidden", "true");
+	$overlay.setAttribute("aria-hidden", "true");
+	$body.classList.remove("no-scroll");
 }
 
 function send(e) {
