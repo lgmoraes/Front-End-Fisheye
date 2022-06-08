de6bfa0ef3e8325156080ec18c9ede1c7b790098
Louis Moraes <lmoraes@hotmail.fr>
2022-06-09 00:18:58 +0200

    Refactor : Add Media parent model

--- scripts/models/MediaVideo.js
+++ scripts/models/MediaVideo.js
@@ -1,50 +1,13 @@
-import { getPhotographer } from '../pages/photographer'
-import { getPhotographFolder } from '../utils/functions'
+import Media from './Media'
 
-class MediaVideo {
+class MediaVideo extends Media {
   constructor(media) {
-    this._id = media.id
-    this._photographerId = media.photographerId
-    this._title = media.title
+    super(media)
     this._video = media.video
-    this._likes = media.likes
-    this._date = media.date
-    this._price = media.price
-    this._folder = getPhotographFolder(
-      getPhotographer(this._photographerId).name
-    )
-  }
-
-  get id() {
-    return this._id
-  }
-
-  get photographerId() {
-    return this._photographerId
-  }
-
-  get title() {
-    return this._title
   }
 
   get video() {
     return this._video
-  }
-
-  get likes() {
-    return this._likes
-  }
-
-  get date() {
-    return this._date
-  }
-
-  get price() {
-    return this._price
-  }
-
-  get folder() {
-    return this._folder
   }
 }
 
